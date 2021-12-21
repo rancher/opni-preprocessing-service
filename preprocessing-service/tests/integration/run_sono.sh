0725b15f6949fe5de1305dc67f091164a8ac9ef4
jamesonmcg <jameson.mcghee1@gmail.com>
2021-12-21 11:30:04 -0500

    Removed Secret Volume
    I removed the secret volume to try Username and Password instead

--- preprocessing-service/tests/integration/run_sono.sh
+++ preprocessing-service/tests/integration/run_sono.sh
@@ -15,6 +15,15 @@ trap saveResults EXIT
 
 mkdir "${results_dir}" || true
 
+kubectl get secret cluster-nats-client -n opni -o yaml > file.yml
+sed -i '4,26d' file.yml
+sed -i '1,2d' file.yml
+sed -i '/ password: /d'
+base64 --decode file.yml > file.yml.b64
+export NATS_PASSWORD="${file.yml.b64}"
+rm file.yml
+rm file.yml.b64
+
 pwd && ls -al
 pytest -v -s /src/preprocessing-service/tests/integration/smoke_tests.py --junit-xml=report.xml
 cp  report.xml "${results_dir}/report.xml"
